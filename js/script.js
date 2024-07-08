d8c218c1a6aaf41eb7800d42ba3de767eca7aceb
ann-kathrinhaas <102215660+ann-kathrinhaas@users.noreply.github.com>
2024-07-08 02:17:47 +0200

    added slick slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});

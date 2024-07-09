ceee74c25ddf83c679fe93371b3aa5126ddd8093
ann-kathrinhaas <102215660+ann-kathrinhaas@users.noreply.github.com>
2024-07-09 22:19:23 +0200

    added html, css and js function for navigation

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    scrollToAnchor();
+    setNavItemActive();
+});
+
+function scrollToAnchor() {
+    const offsetScroll = 88;
+
+    $("a[href^='#'], a[href^='/#']").on("click", function (e) {
+        e.preventDefault();
+        const target = this.hash;
+        const $target = $(target);
+
+        $("html, body").stop().animate({
+            scrollTop: $target.offset().top - offsetScroll
+        }, 600, "swing");
+    });
+}
+
+
+function setNavItemActive() {
+    $(".js-nav-item").on("click", function () {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+        } else {
+            $(this).addClass("active");
+            $(this).siblings(".js-nav-item").removeClass("active");
+        }
+    })
+}

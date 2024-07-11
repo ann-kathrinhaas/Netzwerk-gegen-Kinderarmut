dc228619f010bd00701107f94599cfb4d97bc465
ann-kathrinhaas <102215660+ann-kathrinhaas@users.noreply.github.com>
2024-07-11 17:48:23 +0200

    test slick slider

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
+// $(document).ready(function () {
+//     scrollToAnchor();
+//     setNavItemActive();
+// });
+
+// function scrollToAnchor() {
+//     const offsetScroll = 88;
+
+//     $("a[href^='#'], a[href^='/#']").on("click", function (e) {
+//         e.preventDefault();
+//         const target = this.hash;
+//         const $target = $(target);
+
+//         $("html, body").stop().animate({
+//             scrollTop: $target.offset().top - offsetScroll
+//         }, 600, "swing");
+//     });
+// }
+
+
+// function setNavItemActive() {
+//     $(".js-nav-item").on("click", function () {
+//         if ($(this).hasClass("active")) {
+//             $(this).removeClass("active");
+//         } else {
+//             $(this).addClass("active");
+//             $(this).siblings(".js-nav-item").removeClass("active");
+//         }
+//     })
+// }
+
 $(document).ready(function () {
-    scrollToAnchor();
-    setNavItemActive();
-});
-
-function scrollToAnchor() {
-    const offsetScroll = 88;
-
-    $("a[href^='#'], a[href^='/#']").on("click", function (e) {
-        e.preventDefault();
-        const target = this.hash;
-        const $target = $(target);
-
-        $("html, body").stop().animate({
-            scrollTop: $target.offset().top - offsetScroll
-        }, 600, "swing");
+    $('.slider').slick({
+        infinite: true,
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
     });
-}
-
-
-function setNavItemActive() {
-    $(".js-nav-item").on("click", function () {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-        } else {
-            $(this).addClass("active");
-            $(this).siblings(".js-nav-item").removeClass("active");
-        }
-    })
-}
+});
